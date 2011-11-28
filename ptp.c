0da10251157795e05f4443057490945e0c019e6c
reyalp <reyalp@localhost>
2011-11-28 05:05:01 +0000

    experimental workaround for vxworks file upload bug, see http://chdk.setepontos.com/index.php?topic=6730.15

--- ptp.c
+++ ptp.c
@@ -1711,7 +1711,7 @@ int ptp_chdk_upload(char *local_fn, char *remote_fn, PTPParams* params, PTPDevic
   PTPContainer ptp;
   char *buf = NULL;
   FILE *f;
-  int s,l;
+  unsigned file_len,data_len,file_name_data_len,file_name_len;
 
   PTP_CNT_INIT(ptp);
   ptp.Code=PTP_OC_CHDK;
@@ -1726,18 +1726,29 @@ int ptp_chdk_upload(char *local_fn, char *remote_fn, PTPParams* params, PTPDevic
   }
 
   fseek(f,0,SEEK_END);
-  s = ftell(f);
+  file_len = ftell(f);
   fseek(f,0,SEEK_SET);
 
-  l = strlen(remote_fn);
-  buf = malloc(4+l+s);
-  memcpy(buf,&l,4);
-  memcpy(buf+4,remote_fn,l);
-  fread(buf+4+l,1,s,f);
+  file_name_len = strlen(remote_fn);
+  // make data word aligned by padding filename with extra nulls
+  // (some ?) vxworks cams seem to barf on multiple calls recv_ptp_data if the preceding ones aren't aligned
+  if(file_name_len & 0x3) {
+	  file_name_data_len = (file_name_len & 0xFFFFFFFC) + 0x4;
+  } else {
+	  file_name_data_len = file_name_len;
+  }
+  data_len = 4 + file_name_data_len + file_len;
+  buf = malloc(data_len);
+  memcpy(buf,&file_name_data_len,4);
+  memcpy(buf+4,remote_fn,file_name_len);
+  if(file_name_len != file_name_data_len) {
+	  memset(buf+4+file_name_len,0,file_name_data_len - file_name_len);
+  }
+  fread(buf+4+file_name_data_len,1,file_len,f);
 
   fclose(f);
 
-  ret=ptp_transaction(params, &ptp, PTP_DP_SENDDATA, 4+l+s, &buf);
+  ret=ptp_transaction(params, &ptp, PTP_DP_SENDDATA, data_len, &buf);
 
   free(buf);
 
